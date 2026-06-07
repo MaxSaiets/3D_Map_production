87036c1ea69c7b02e80f41c51457276b0547f66d
saets.m <saets.m@hexide-digital.com>
2026-06-07 08:15:39 +0300

    fix(i18n): remove nested robocopy duplicate dirs (i18n/i18n, messages/messages)

--- frontend/i18n/i18n/i18n/navigation.ts
+++
@@ -1,6 +0,0 @@
-import { createNavigation } from "next-intl/navigation";
-import { routing } from "./routing";
-
-// Locale-aware Link / useRouter / usePathname / redirect.
-export const { Link, redirect, usePathname, useRouter, getPathname } =
-  createNavigation(routing);
